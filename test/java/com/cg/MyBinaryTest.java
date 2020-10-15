d91361e5c3fc7df033f3c114b33045662e6529e1
adityak-bit <adityajsr1998@gmail.com>
2020-10-15 07:41:52 +0530

    [ADITYA] Checked if all are added using size() in BST

--- test/java/com/cg/MyBinaryTest.java
+++ test/java/com/cg/MyBinaryTest.java
@@ -9,9 +9,22 @@ public class MyBinaryTest {
 	public void given3NumbersWhenAddedToBinaryShouldReturnSizeThree() {
 		MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
 		myBinaryTree.add(56);
-		myBinaryTree.add(30);
-		myBinaryTree.add(70);
+		//left subtree
+		{myBinaryTree.add(30);
+		{myBinaryTree.add(22);
+		{myBinaryTree.add(11);
+		myBinaryTree.add(3);
+		myBinaryTree.add(16);}}
+		myBinaryTree.add(40);}
+		//Right subtree
+		{myBinaryTree.add(70);
+		{myBinaryTree.add(60);
+		{myBinaryTree.add(65);
+		myBinaryTree.add(63);
+		myBinaryTree.add(67);}}
+		myBinaryTree.add(95);}
+		
 		int size = myBinaryTree.getSize();
-		Assert.assertEquals(3, size);
+		Assert.assertEquals(13, size);
 	}
 }
